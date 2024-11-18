9b40b958abb62740991c14258345359453ca7186
Sergey-Yakushev <137285867+Sergey-Yakushev@users.noreply.github.com>
2024-11-18 14:05:29 +0300

    taxi

---
+++ taxi.js
@@ -0,0 +1,152 @@
+
+const inputAc = document.querySelectorAll('.taxi__input');
+const linkOp = document.querySelectorAll('.copp__tax');
+const taxi = document.querySelector('form')
+
+
+
+for (let i = 0; i < inputAc.length; i++) {
+  linkOp[i].addEventListener('click', function () {
+    const qq = inputAc[i].value;
+    navigator.clipboard.writeText(qq);
+  });
+}
+
+function inputActiv() {
+  for (let i = 0; i < inputAc.length; i++) {
+    inputAc[i].addEventListener('input', function () {
+      if (this.value !== '') {
+        this.style.backgroundColor = "#49d831d4";
+      } else if (this.value == '') {
+        this.style.removeProperty("background-color");
+      }
+    })
+  }
+}
+inputActiv()
+
+// const taxi = document.querySelector('form')
+const ls = localStorage;
+let formData = {};
+
+taxi.addEventListener('input', function(event) {
+  formData[event.target.name] = event.target.value;
+  ls.setItem('formData', JSON.stringify(formData))
+})
+
+if (ls.getItem('formData')) {
+  formData = JSON.parse(ls.getItem('formData'));
+  for (let key in formData) {
+    taxi.elements[key].value = formData[key];
+  }
+  inputActiv()
+}
+
+
+
+
+
+
+
+
+
+
+
+// const textTaxi = document.querySelectorAll('.taxi__input');
+// const coppTaxi = document.querySelectorAll('[data-type]')
+// const inputElemnt = document.querySelector('.add__text')
+// const creatBtn = document.querySelector('.add__btn')
+// const taxiList = document.querySelector('.copp__wrapper')
+
+// const taxiWrapper = document.querySelector('.taxi__wrapper');
+// const ls = localStorage;
+
+
+
+
+// // localStorage.setItem('taxiWrapper', JSON.stringify(taxiWrapper.innerHTML));
+// // let b = localStorage.getItem('taxiWrapper');
+// // b = JSON.parse(b)
+// // console.log(b);
+// // // console.log(b);
+
+// // ls.setItem('taxiWrapper', JSON.stringify(taxiWrapper.innerHTML));
+
+// // let b = ls.getItem('taxiWrapper', taxiWrapper);
+// // b = JSON.parse(b);
+
+
+
+// const notes = []
+
+// function render() {
+//   taxiList.innerHTML = '';
+//   for (let i = 0; i < notes.length; i++) {
+//     taxiList.insertAdjacentHTML('beforeend', getNoteTamplate(notes[i], i, i))
+//   }
+
+
+// }
+// render()
+
+// creatBtn.addEventListener('click', function () {
+//   if (inputElemnt.value.length === 0) {
+//     return
+//   }
+//   const newNote = {
+//     title: inputElemnt.value
+//   }
+//   notes.push(newNote)
+//   render()
+//   inputElemnt.value = '';
+
+// })
+
+
+// taxiList.addEventListener('click', function (event) {
+//   // console.log(event.target.dataset)
+//   if (event.target.dataset.index) {
+//     const index = parseInt(event.target.dataset.index);
+//     const type = event.target.dataset.type;
+//     if (type === 'delete') {
+//       notes.splice(index, 1)
+//     }
+//   }
+//   render()
+// })
+
+
+
+// function getNoteTamplate(note, index) {
+//   return `
+//    <div class="copp__wrapper">
+//   <span class="taxi__input" data-index="${index}">${note.title}</span>
+//   <img class="copp__tax" data-type="cop" data-index="${index}" src="/images/copy-alt-svgrepo-com.svg" alt="">
+//   </div>
+//   <div class="img__taxi">
+//     <img class="cross" data-type="delete" data-index="${index}" src="/images/cross.svg" alt="">
+//   </div>
+// </div>
+// `
+// }
+// const notes = []
+// function notesItem(index) {
+//   return `
+//    <div class="taxi">
+//         <!-- <div class="copp__wrapper">
+//           <div class="copp__wrapper">
+//             <input type="text" class="taxi__input" ></span>
+//             <img class="copp__tax" src="/images/copy-alt-svgrepo-com.svg" alt="">
+//           </div> -->
+//       </div>
+//   `
+// }
+
+// function render() {
+//   for (let i = 0; i < notes.length; i++) {
+//     taxiWrapper.innerHTML = "";
+//     taxiWrapper.insertAdjacentHTML('beforeend', notesItem(notes[i],))
+//   }
+// }
+// console.log('taxiWrapper')
+// render()
